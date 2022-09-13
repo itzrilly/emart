2890dbe3860c29a8360416c224bd80ae693908b3
RILLY <mac@MacBook-Pro-de-RILLY.local>
2022-09-13 12:50:52 +0100

    Configuration of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Products from './components/Products';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route exac path='/' element={<Home/>} />
+        <Route exac path='/products' element={<Products/>} />
+      </Routes>
     </>
   );
 }
